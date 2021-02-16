ec0b00f4f756408a943b1db47016319037cba2c2
LindaBetz <linda.betz@uk-koeln.de>
2021-02-16 11:32:52 +0100

    add files

---
+++ Code_Main_Analysis.R
@@ -0,0 +1,589 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~     Code for    ~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#
+#
+#
+#
+#
+#                   - Analysis reported in main manuscript -
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# -------------------------- 0: Reproducibility -----------------------------
+
+# for reproducibility, one can use the "checkpoint" package
+# in a temporary directory, it will *install* those package versions used when the script was written
+# these versions are then used to run the script
+# to this end, a server with snapshot images of archived package versions needs to be contacted
+# for more info visit: https://mran.microsoft.com/documents/rro/reproducibility
+
+library(checkpoint)
+checkpoint(
+  snapshotDate = "2021-02-08",
+  R.version = "4.0.3",
+  checkpointLocation = tempdir()
+)
+
+# -------------------------- 1: Load packages & data -----------------------------
+# ------- 1.1 load packages -------
+library(haven)
+library(networktree)
+library(bootnet)
+library(qgraph)
+library(BGGM)
+library(tidyverse)
+
+# ------- 1.2 define custom functions -------
+global_strength <- function(data1, data2) {
+  # estimate graphs
+  fit1 <-  BGGM::estimate(data1, analytic = TRUE)
+  fit2 <-  BGGM::estimate(data2, analytic = TRUE)
+  
+  # select graphs
+  sel1 <-
+    BGGM::select(fit1, cred = 0) # cred = 0 => no "pruning" of edges
+  sel2 <- BGGM::select(fit2, cred = 0)
+  
+  abs(sum(abs(sel1$pcor_adj[upper.tri(sel1$pcor_adj)])) - sum(abs(sel2$pcor_adj[upper.tri(sel2$pcor_adj)])))
+}
+
+# custom function to compare networks in relevant aspects (global strength, individual connections)
+# & store results
+compare_networks <- function(data1,
+                             data2,
+                             iter = 5000,
+                             node_names =  c("worry" , "sleep_pr", "anx", "depr", "per", "hal")) {
+  individual_edges <- ggm_compare_estimate(data1,
+                                           data2,
+                                           progress = FALSE,
+                                           iter = iter,
+                                           seed = 1)
+  
+  individual_edges <-
+    as.data.frame(BGGM::select(individual_edges, cred = 0.95)$pcor_adj)
+  colnames(individual_edges) <-
+    rownames(individual_edges) <- node_names
+  
+  
+  obs_gs <-
+    global_strength(data1,
+                    data2)
+  
+  res_gs <- ggm_compare_ppc(
+    data1,
+    data2,
+    FUN = global_strength,
+    iter = iter,
+    custom_obs  = obs_gs,
+    progress = FALSE
+  )
+  
+  return(
+    list(
+      individual_edges = individual_edges,
+      global_strength_diff = obs_gs,
+      global_strength_p_val = res_gs$ppp_custom
+    )
+  )
+}
+
+# ------- 1.3 load 2007 AMPS data -------
+apms07arch <-
+  read_sav("UKDA-6379-spss/spss/spss19/apms07arch.sav")
+
+# ------- 1.4 data preparation -------
+data_apms <- apms07arch %>%
+  transmute(
+    # symptoms
+    worry = case_when(I10 > 1 ~ 1, is.na(I1) ~ NA_real_, TRUE ~ 0),
+    sleep_pr = case_when(D10 > 1 ~ 1,  is.na(D1) ~ NA_real_, TRUE ~ 0),
+    anx = case_when(J11 > 1 ~ 1, is.na(J1) ~ NA_real_, TRUE ~ 0),
+    depr = case_when(G10 > 1 ~ 1, is.na(G1) ~ NA_real_, TRUE ~ 0),
+    # psychosis
+    per = ifelse(PSQ3 == 1 & PSQ3a == 1, 1, 0),
+    hal = ifelse(PSQ5 == 1 & PSQ5a == 1, 1, 0),
+    # potential moderators/split variables
+    age = as.numeric(ResAge),
+    sex = as.factor(ifelse(ResSex == 2, "female", "male")),
+    bullying =  as.factor(
+      case_when(Trauma31 == 1 ~ "yes",
+                is.na(Trauma31) ~ NA_character_,
+                TRUE ~ "no")
+    ),
+    violence =  as.factor(
+      case_when(Trauma33 == 1 ~ "yes",
+                is.na(Trauma33) ~ NA_character_,
+                TRUE ~ "no")
+    ),
+    separation = as.factor(
+      case_when(
+        LACare == 1 | ChldInst == 1 ~ "yes",
+        is.na(LACare) & is.na(ChldInst) ~ NA_character_,
+        TRUE ~ "no"
+      )
+    ),
+    sexual_abuse = as.factor(
+      case_when(
+        VBa == 1 | VBb == 1 | VBc == 1 ~ "yes",
+        is.na(VBa) &
+          is.na(VBb) & is.na(VBc) & is.na(VBd) ~ NA_character_,
+        TRUE ~ "no"
+      )
+    ),
+    physical_abuse = as.factor(
+      case_when(
+        VBd == 1 ~ "yes",
+        is.na(VBa) &
+          is.na(VBb) & is.na(VBc) & is.na(VBd) ~ NA_character_,
+        TRUE ~ "no"
+      )
+    ),
+    cannabis = as.factor(ifelse(Cannyear == 1, "yes", "no")),
+    alcohol = as.numeric(DVAudit1),
+    ethnicity = as.factor(ETHNIC4),
+    deprivation = as.numeric(qimd)
+  )
+
+# -------------------------- 2: Sample descriptives --------------------------
+# ------- n participants with missing data
+data_apms %>%
+  mutate(any_NA = rowSums(is.na(.))) %>%
+  filter(any_NA > 0) %>% nrow(.) # 146 participants have any variable missing
+
+# ------- % participants with missing data
+146 / nrow(data_apms)  # 0.01972173
+
+
+data_missings_removed <- data_apms  %>% na.omit()
+nrow(data_missings_removed) # 7257
+
+# ------- symptom prevalence (table 1)
+data_missings_removed %>%
+  select(-c(age, alcohol, deprivation)) %>%
+  summarise(across(where(is.numeric), ~ mean(.x)))
+
+
+# ------- moderator descriptive statistics (table 1)
+data_missings_removed %>% select(where(is.factor)) %>%
+  map(., ~ table(.) /
+        nrow(data_missings_removed))
+
+data_missings_removed %>%
+  select(age, alcohol, deprivation) %>%
+  summarise(across(everything(), c(median = median, IQR = IQR)))
+
+
+
+data_missings_removed %>% select(-c(
+  "worry",
+  "sleep_pr" ,
+  "anx"  ,
+  "depr"   ,
+  "per",
+  "hal" ,
+  "age" ,
+  "sex",
+  "alcohol"
+)) %>%
+  mutate(
+    ethnicity = ifelse(ethnicity == 1, "no", "yes"),
+    deprivation = ifelse(deprivation == 5, "yes", "no")
+  ) %>%
+  mutate_all(~ ifelse(. == "yes", 1, 0)) %>%
+  mutate_all(as.integer) %>%
+  as.data.frame(.) %>%
+  upset(
+    .,
+    nsets = 10,
+    matrix.color = "steelblue",
+    main.bar.color = "steelblue",
+    sets.bar.color = "steelblue"
+  )
+
+
+# -------------------------- 3: Network Estimation --------------------------
+# -------------- estimate & plot network based on whole sample
+node_names <-
+  c(
+    "worry",
+    "sleep disturbance",
+    "anxiety",
+    "depression",
+    "persecutory ideation",
+    "hallucinatory experiences"
+  )
+
+png(filename = "whole_sample.png",
+    width = 300,
+    height = 300)
+qgraph(
+  cor(data_missings_removed %>% .[, 1:6],
+      method = "pearson"),
+  graph = "pcor",
+  vsize = 10,
+  edge.width = 1.75,
+  label.cex = 2.25,
+  cut = 0,
+  borders = T,
+  height = 1,
+  width = 1,
+  minimum = 0.01,
+  maximum = 0.1,
+  theme = "colorblind",
+  layout = "circle",
+  labels = c("1", "2", "3", "4", "5", "6"),
+  color =  c(rep("#f7f5f2", 5),
+             rep("#c5ceed", 2))
+)
+dev.off()
+
+# -------------------------- 4: Recursive Partitioning --------------------------
+
+set.seed(234)
+apms_networktree <- networktree(
+  nodevars = data_missings_removed[, 1:6],
+  # symptoms
+  splitvars = data_missings_removed[, 7:ncol(data_missings_removed)],
+  # risk factors
+  method = "mob",
+  #default method
+  model = c("correlation"),
+  transform = "pcor",
+  minsize = ceiling(nrow(data_missings_removed) * 0.01)
+)
+
+# plotting for overview only; actual figure 2 was made in PowerPoint
+plot(
+  apms_networktree,
+  vsize = 10,
+  edge.width = 2.5,
+  label.cex = 2.5,
+  cut = 0,
+  borders = T,
+  height = 1,
+  width = 1,
+  minimum = 0.01,
+  maximum = 0.1,
+  theme = "colorblind",
+  layout = "lock",
+  labels = c("1", "2", "3", "4", "5", "6"),
+  color =  c(rep("#f7f5f2", 4),
+             rep("#c5ceed", 2))
+)
+
+# ---------------------------------- 5: Subgroup Differences -----------------------------------
+# ------ 5.1: first split: women vs. men ------
+# descriptive comparison
+comparetree(apms_networktree,
+            id1 = 2,
+            id2 = 5,
+            highlights = 3)
+
+# statistical comparison
+set.seed(123)
+first_split <-
+  compare_networks(
+    data_missings_removed %>% filter(sex == "female") %>% .[, 1:6],
+    data_missings_removed %>% filter(sex == "male") %>% .[, 1:6]
+  )
+
+# ------ 5.2: second split: cannabis use in women ------
+# descriptive comparison
+comparetree(apms_networktree,
+            id1 = 4,
+            id2 = 3,
+            highlights = 3)
+
+# statistical comparison
+set.seed(123)
+second_split <-
+  compare_networks(
+    data_missings_removed %>% filter(sex == "female" &
+                                       sexual_abuse == "yes") %>% .[, 1:6],
+    data_missings_removed %>% filter(sex == "female" &
+                                       sexual_abuse == "no") %>% .[, 1:6]
+  )
+
+png(filename = "women_sexual_abuse_yes.png",
+    width = 300,
+    height = 300)
+qgraph(
+  cor(
+    data_missings_removed %>% filter(sex == "female" &
+                                       sexual_abuse == "yes") %>% .[, 1:6],
+    method = "pearson"
+  ),
+  graph = "pcor",
+  vsize = 13,
+  edge.width = 1.75,
+  label.cex = 2.25,
+  cut = 0,
+  borders = T,
+  height = 1,
+  width = 1,
+  minimum = 0.01,
+  maximum = 0.1,
+  theme = "colorblind",
+  layout = "circle",
+  labels = c("1", "2", "3", "4", "5", "6"),
+  color =  c(rep("#f7f5f2", 4),
+             rep("#c5ceed", 2))
+)
+dev.off()
+
+png(filename = "women_sexual_abuse_no.png",
+    width = 300,
+    height = 300)
+qgraph(
+  cor(
+    data_missings_removed %>% filter(sex == "female" &
+                                       sexual_abuse == "no") %>% .[, 1:6],
+    method = "pearson"
+  ),
+  graph = "pcor",
+  vsize = 13,
+  edge.width = 1.75,
+  label.cex = 2.25,
+  cut = 0,
+  borders = T,
+  height = 1,
+  width = 1,
+  minimum = 0.01,
+  maximum = 0.1,
+  theme = "colorblind",
+  layout = "circle",
+  labels = c("1", "2", "3", "4", "5", "6"),
+  color =  c(rep("#f7f5f2", 4),
+             rep("#c5ceed", 2))
+)
+dev.off()
+
+
+# ------ 5.3: third split: domestic violence in men ------
+# descriptive comparison
+comparetree(apms_networktree,
+            id1 = 9,
+            id2 = 4,
+            highlights = 3)
+
+# statistical comparison
+set.seed(123)
+fourth_split <-
+  compare_networks(
+    data_missings_removed %>% filter(sex == "male" &
+                                       violence == "yes") %>% .[, 1:6],
+    data_missings_removed %>% filter(sex == "male" &
+                                       violence == "no") %>% .[, 1:6]
+  )
+
+png(filename = "men_domestic_violence_yes.png",
+    width = 300,
+    height = 300)
+qgraph(
+  cor(
+    data_missings_removed %>% filter(sex == "male" &
+                                       violence == "yes") %>% .[, 1:6],
+    method = "pearson"
+  ),
+  graph = "pcor",
+  vsize = 13,
+  edge.width = 1.75,
+  label.cex = 2.25,
+  cut = 0,
+  borders = T,
+  height = 1,
+  width = 1,
+  minimum = 0.01,
+  maximum = 0.1,
+  theme = "colorblind",
+  layout = "circle",
+  labels = c("1", "2", "3", "4", "5", "6"),
+  color =  c(rep("#f7f5f2", 4),
+             rep("#c5ceed", 2))
+)
+dev.off()
+
+# ------  5.4: third split: cannabis in men (in violence=no) ------
+# descriptive comparison
+comparetree(apms_networktree,
+            id1 = 6,
+            id2 = 5,
+            highlights = 3)
+
+# statistical comparison
+set.seed(123)
+fifth_split <-
+  compare_networks(
+    data_missings_removed %>% filter(sex == "male" &
+                                       violence == "no" &
+                                       cannabis == "yes") %>% .[, 1:6],
+    data_missings_removed %>% filter(sex == "male" &
+                                       violence == "no" &
+                                       cannabis == "no") %>% .[, 1:6]
+  )
+
+# plotting the respective subgroup network
+png(filename = "men_cannabis_yes.png",
+    width = 300,
+    height = 300)
+qgraph(
+  cor(
+    data_missings_removed %>% filter(sex == "male" &
+                                       violence == "no" &
+                                       cannabis == "yes") %>% .[, 1:6],
+    method = "pearson"
+  ),
+  graph = "pcor",
+  vsize = 13,
+  edge.width = 1.75,
+  label.cex = 2.25,
+  cut = 0,
+  borders = T,
+  height = 1,
+  width = 1,
+  minimum = 0.01,
+  maximum = 0.1,
+  theme = "colorblind",
+  layout = "circle",
+  labels = c("1", "2", "3", "4", "5", "6"),
+  color =  c(rep("#f7f5f2", 4),
+             rep("#c5ceed", 2))
+)
+dev.off()
+
+
+# ------  5.4: third split: cannabis in men (in violence=no) ------
+# descriptive comparison
+comparetree(apms_networktree,
+            id1 = 8,
+            id2 = 9,
+            highlights = 3)
+
+# statistical comparison
+set.seed(123)
+fifth_split <-
+  compare_networks(
+    data_missings_removed %>% filter(sex == "male" &
+                                       violence == "no" &
+                                       cannabis == "no" & ethnicity %in% c(2, 3)) %>% .[, 1:6],
+    data_missings_removed %>% filter(sex == "male" &
+                                        violence == "no" &
+                                       cannabis == "no" & ethnicity %in% c(1, 4)) %>% .[, 1:6]
+  )
+
+# plotting the respective subgroup network
+png(filename = "men_ethnic_minority.png",
+    width = 300,
+    height = 300)
+qgraph(
+  cor(
+    data_missings_removed %>% filter(sex == "male" &
+                                       violence == "no" &
+                                       cannabis == "no" & ethnicity %in% c(2, 3)) %>% .[, 1:6],
+    
+    method = "pearson"
+  ),
+  graph = "pcor",
+  vsize = 13,
+  edge.width = 1.75,
+  label.cex = 2.25,
+  cut = 0,
+  borders = T,
+  height = 1,
+  width = 1,
+  minimum = 0.01,
+  maximum = 0.1,
+  theme = "colorblind",
+  layout = "circle",
+  labels = c("1", "2", "3", "4", "5", "6"),
+  color =  c(rep("#f7f5f2", 4),
+             rep("#c5ceed", 2))
+)
+dev.off()
+
+# ------ 5.7: seventh split: ethnicity ------
+# descriptive comparison
+comparetree(ets,
+            id1 = 8,
+            id2 = 5,
+            highlights = 3)
+
+# statistical comparison
+set.seed(123)
+fifth_split <-
+  compare_networks(
+    data_missings_removed %>% filter(
+      sex == "male" &
+         violence == "no" &
+        cannabis == "no" &
+        ethnicity %in% c(1, 4) & bullying == "yes"
+    ) %>% .[, 1:6],
+    data_missings_removed %>% filter(
+      sex == "male" &
+         violence == "no" &
+        cannabis == "no" & ethnicity %in% c(1, 4) & bullying == "no"
+    ) %>% .[, 1:6]
+  )
+
+png(filename = "men_ethnic_majority_bullying_yes.png",
+    width = 300,
+    height = 300)
+qgraph(
+  cor(
+    data_missings_removed %>% filter(
+      sex == "male" &
+        violence == "no" &
+        cannabis == "no" &
+        ethnicity %in% c(1, 4) & bullying == "yes"
+    ) %>% .[, 1:6],
+    
+    method = "pearson"
+  ),
+  graph = "pcor",
+  vsize = 13,
+  edge.width = 1.75,
+  label.cex = 2.25,
+  cut = 0,
+  borders = T,
+  height = 1,
+  width = 1,
+  minimum = 0.01,
+  maximum = 0.1,
+  theme = "colorblind",
+  layout = "circle",
+  labels = c("1", "2", "3", "4", "5", "6"),
+  color =  c(rep("#f7f5f2", 4),
+             rep("#c5ceed", 2))
+)
+dev.off()
+
+
+png(filename = "men_ethnic_majority_bullying_no.png",
+    width = 300,
+    height = 300)
+qgraph(
+  cor(
+    data_missings_removed %>% filter(
+      sex == "male" &
+        violence == "no" &
+        cannabis == "no" &
+        ethnicity %in% c(1, 4) & bullying == "no"
+    ) %>% .[, 1:6],
+    
+    method = "pearson"
+  ),
+  graph = "pcor",
+  vsize = 13,
+  edge.width = 1.75,
+  label.cex = 2.25,
+  cut = 0,
+  borders = T,
+  height = 1,
+  width = 1,
+  minimum = 0.01,
+  maximum = 0.1,
+  theme = "colorblind",
+  layout = "circle",
+  labels = c("1", "2", "3", "4", "5", "6"),
+  color =  c(rep("#f7f5f2", 4),
+             rep("#c5ceed", 2))
+)
+dev.off()
---
+++ Code_Supplementary_Analyses.R
@@ -0,0 +1,257 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~     Code for    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#                                                                                                     #
+#            #             #
+#                         #                           #
+#                                     developed by L. Betz                                            #
+#                                                                                                     #
+#                               - Analysis reported in supplementaries -                              #
+#                                                                                                     #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ---------------------------------- 0: Reproducibility  -----------------------------------
+
+# for reproducibility, one can use the "checkpoint" package
+# in a temporal directory, it will *install* those package versions used when the script was written
+# these versions are then used to run the script
+# to this end, a server with snapshot images of archived package versions needs to be contacted
+# for more info visit: https://mran.microsoft.com/documents/rro/reproducibility
+
+library(checkpoint)
+checkpoint(
+  snapshotDate = "2020-11-07",
+  R.version = "4.0.3",
+  checkpointLocation = tempdir()
+)
+
+# ---------------------------------- 1: Load packages & data -----------------------------------
+# ------- 1.1 load packages -------
+library(haven)
+library(networktree)
+library(bootnet)
+library(qgraph)
+library(BGGM)
+library(coin)
+library(tidyverse)
+
+# ------- 1.2 define custom function -------
+check_stability <- function(data, iters = 5000) {
+  out <-  estimateNetwork(data, default = "pcor", alpha = 1)
+  set.seed(1)
+  boot_out <-
+    bootnet(
+      out,
+      type = "case",
+      statistics = "edge",
+      iters = iters,
+      verbose = FALSE,
+      replacement = TRUE
+    )
+  res <- corStability(boot_out, verbose = FALSE)
+  return(res)
+}
+
+# ------- 1.3 load 2007 AMPS data -------
+apms07arch <-
+  read_sav("UKDA-6379-spss/spss/spss19/apms07arch.sav")
+
+# ------- 1.4 data preparation -------
+# ------- 1.4 data preparation -------
+data_apms <- apms07arch %>%
+  transmute(
+    # symptoms
+    worry = case_when(I10 > 1 ~ 1, is.na(I1) ~ NA_real_, TRUE ~ 0),
+    sleep_pr = case_when(D10 > 1 ~ 1,  is.na(D1) ~ NA_real_, TRUE ~ 0),
+    anx = case_when(J11 > 1 ~ 1, is.na(J1) ~ NA_real_, TRUE ~ 0),
+    depr = case_when(G10 > 1 ~ 1, is.na(G1) ~ NA_real_, TRUE ~ 0),
+    # psychosis
+    per = ifelse(PSQ3 == 1 & PSQ3a == 1, 1, 0),
+    hal = ifelse(PSQ5 == 1 & PSQ5a == 1, 1, 0),
+    # potential moderators/split variables
+    age = as.numeric(ResAge),
+    sex = as.factor(ifelse(ResSex == 2, "female", "male")),
+    bullying =  as.factor(
+      case_when(Trauma31 == 1 ~ "yes",
+                is.na(Trauma31) ~ NA_character_,
+                TRUE ~ "no")
+    ),
+    violence =  as.factor(
+      case_when(Trauma33 == 1 ~ "yes",
+                is.na(Trauma33) ~ NA_character_,
+                TRUE ~ "no")
+    ),
+    separation = as.factor(
+      case_when(
+        LACare == 1 | ChldInst == 1 ~ "yes",
+        is.na(LACare) & is.na(ChldInst) ~ NA_character_,
+        TRUE ~ "no"
+      )
+    ),
+    sexual_abuse = as.factor(
+      case_when(
+        VBa == 1 | VBb == 1 | VBc == 1 ~ "yes",
+        is.na(VBa) &
+          is.na(VBb) & is.na(VBc) & is.na(VBd) ~ NA_character_,
+        TRUE ~ "no"
+      )
+    ),
+    physical_abuse = as.factor(
+      case_when(
+        VBd == 1 ~ "yes",
+        is.na(VBa) &
+          is.na(VBb) & is.na(VBc) & is.na(VBd) ~ NA_character_,
+        TRUE ~ "no"
+      )
+    ),
+    cannabis = as.factor(ifelse(Cannyear == 1, "yes", "no")),
+    alcohol = as.numeric(DVAudit1),
+    ethnicity = as.factor(ETHNIC4),
+    deprivation = as.numeric(qimd)
+  )
+
+data_missings_removed <- data_apms  %>% na.omit()
+
+
+# ---------------------------------- 2: Supplementary Table 1: differences in included vs. excluded participants  -----------------------------------
+# ------- 2.1 data preparation -------
+data_included_excluded <- data_apms %>%
+  mutate(included_excluded = if_else(rowSums(is.na(.)) > 0, "excluded", "included"))
+
+# ------- 2.2 descriptive statistics -------
+# interval data
+data_included_excluded %>%
+  select(., c(age, alcohol, deprivation, included_excluded)) %>%
+  group_by(included_excluded) %>%
+  summarise(across(everything(), c(
+    median = ~ median(.x, na.rm = TRUE),
+    IQR = ~ IQR(.x, na.rm = TRUE)
+  )))
+
+# frequency data
+data_included_excluded %>%
+  select(.,-c(age, alcohol, deprivation, ethnicity)) %>%
+  mutate(across(
+    where(is.factor),
+    ~ case_when(
+      . %in% c("yes", "female") ~ 1,
+      . %in% c("no", "male") ~ 0,
+      TRUE ~ NA_real_
+    )
+  )) %>%
+  group_by(included_excluded) %>%
+  summarise(across(everything(), ~ mean(.x, na.rm = TRUE))) %>%
+  mutate(across(where(is.numeric), ~ round(., 3)))
+
+excluded <- data_included_excluded %>% 
+  filter(included_excluded=="excluded") 
+
+round(table(excluded$ethnicity)/96, 3)
+
+# ------- 2.3 statistical comparison -------
+# interval data
+set.seed(1)
+data_included_excluded %>%
+  select(., c(age, alcohol, deprivation)) %>%
+  map(
+    ~ wilcox_test(
+      as.numeric(.) ~ as.factor(data_included_excluded$included_excluded),
+      data = data_included_excluded,
+      distribution = "approximate"
+    )
+  )
+
+# frequency data
+set.seed(1)
+data_included_excluded %>%
+  select(., -c(age, alcohol, deprivation, included_excluded)) %>%
+  map(
+    ~ chisq_test(
+      as.factor(.) ~ as.factor(data_included_excluded$included_excluded),
+      data = data_included_excluded,
+      distribution = "approximate"
+    )
+  )
+
+# ---------------------------------- 3: Supplementary Table 2: stability of results -----------------------------------
+# ------ 3.1 full sample ------
+full_sample <-
+  check_stability(data_missings_removed %>% .[, 1:6]) # 0.7500344
+
+# ------ 3.2 first split: sex differences ------
+
+first_split_grp1 <-
+  check_stability(data_missings_removed %>% filter(sex == "female") %>% .[, 1:6]) # 0.7500606
+
+
+first_split_grp2 <-
+  check_stability(data_missings_removed %>% filter(sex == "male") %>% .[, 1:6]) # 0.75
+
+# ------ 3.3 second split: childhood sexual abuse in women ------
+
+second_split_grp1 <-
+  check_stability(data_missings_removed %>% filter(sex == "female" &
+                                                     sexual_abuse == "yes") %>% .[, 1:6]) # 0.6728045
+
+second_split_grp2 <-
+  check_stability(data_missings_removed %>% filter(sex == "female" &
+                                                     sexual_abuse == "no") %>% .[, 1:6]) # 0.7499269
+
+# ------ 3.4 third split: in men: domestic violence ------
+
+third_split_grp1 <-
+  check_stability(data_missings_removed %>% filter(sex == "male" &
+                                                     violence == "yes") %>% .[, 1:6]) # 0.4358974
+
+third_split_grp2 <-
+  check_stability(data_missings_removed %>% filter(sex == "male" &
+                                                     violence == "no") %>% .[, 1:6])  # 0.75
+
+# ------ 3.5 fourth split: in men & domestic violence==no: cannabis use ------
+
+fourth_split_grp1 <-
+  check_stability(
+    data_missings_removed %>% filter(sex == "male" & violence == "no" &
+                                       cannabis == "yes") %>% .[, 1:6]
+  ) # 0.4368932
+
+fourth_split_grp2 <-
+  check_stability(data_missings_removed %>% filter(sex == "male" &
+                                                     violence == "no" &
+                                                     cannabis == "no") %>% .[, 1:6]) # 0.7501805
+
+# ------ 3.6 fifth split: in men & domestic violence==no & cannabis==no: ethnicity ------
+fifth_split_grp1 <-
+  check_stability(
+    data_missings_removed %>% filter(
+      sex == "male" & violence == "no" &
+        cannabis == "no" & ethnicity %in% c(1, 4)
+    ) %>% .[, 1:6]
+    
+  ) # 0.7499041
+
+
+fifth_split_grp2 <-
+  check_stability(
+    data_missings_removed %>% filter(
+      sex == "male" & violence == "no" &
+        cannabis == "no" & ethnicity %in% c(2, 3)
+    ) %>% .[, 1:6]
+    
+  ) # 0.5153374
+
+# ------ 3.7 sixth split: in men & domestic violence==no & cannabis==no & ethnicity == 1,4: lifetime bullying  ------
+sixth_split_grp1 <-
+  check_stability(
+    data_missings_removed %>% filter(
+      sex == "male" & violence == "no" &
+        cannabis == "no" &
+        ethnicity %in% c(1, 4) & bullying == "yes"
+    ) %>% .[, 1:6]
+  ) # 0.5943396
+
+sixth_split_grp2 <-
+  check_stability(
+    data_missings_removed %>% filter(
+      sex == "male" & violence == "no" &
+        cannabis == "no" &
+        ethnicity %in% c(1, 4) & bullying == "no"
+    ) %>% .[, 1:6]
+  ) # 0.7498855
